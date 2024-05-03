0f3944f667149c426e5ed34949ef32fde20cc46d
YoRu Tech <tech.niko.coding@gmail.com>
2024-05-03 09:18:59 +0500

    locomotive working successfully

---
+++ src/components/Page01.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Page01 = () => {
+  return (
+    <div className="page">
+      <h1>Page01</h1>
+    </div>
+  );
+};
+
+export default Page01;
